9ed5f94c287bdee2a483f38fd8b33438282ebddc
Tajudeen Fawas <frankokonkwo24@gmail.com>
2026-01-13 20:38:18 +0100

    worked on the settings page

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -1,5 +1,52 @@
+import CategoryManager from "@/components/admin/category";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Users } from "lucide-react";
+
+
 function SettingsPage() {
-  return <div>Settings</div>;
+  return (
+    <div className="container py-10">
+      <h1 className="text-3xl font-bold mb-5">Admin Settings</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mb-7">
+        <Card className="bg-white">
+          <CardHeader className="pb-2">
+            <CardTitle className="flex items-center text-lg font-meddium">
+              <Users className="mr-2 h-5 w-5 text-teal-500" />
+              Total Users
+            </CardTitle>
+            <CardDescription>
+              All registered users on the platform
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="text-3xl font-bold text-teal-600">100</p>
+          </CardContent>
+        </Card>
+        <Card className="bg-white">
+          <CardHeader className="pb-2">
+            <CardTitle className="flex items-center text-lg font-meddium">
+              <Users className="mr-2 h-5 w-5 text-teal-500" />
+              Total Assets
+            </CardTitle>
+            <CardDescription>
+              All uploaded assets on the platform
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="text-3xl font-bold text-teal-600">1000</p>
+          </CardContent>
+        </Card>
+      </div>
+      <Card className="bg-white">
+        <CardHeader>
+          <CardTitle>Category Management</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CategoryManager />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 export default SettingsPage;
---
+++ components/admin/category.tsx
@@ -0,0 +1,10 @@
+function CategoryManager() {
+    return (
+        <div>
+            <h2 className="text-2xl font-bold mb-4">Manage Categories</h2>
+
+        </div>
+    );
+}
+
+export default CategoryManager;
