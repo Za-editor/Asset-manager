05096d15ba1cebdeb4d6106da79594222e5eaa8b
Tajudeen Fawas <frankokonkwo24@gmail.com>
2026-01-13 13:00:04 +0100

    created the schema file using cli

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,26 +1,35 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
+import { admin } from "better-auth/plugins/admin";
+import { nextCookies } from "better-auth/next-js";
 
 const adminRole = "admin";
 const userRole = "user";
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg"
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      mapProfileToUser: (profile) => {
+        return {
+          email: profile.email,
+          name: profile.name,
+          image: profile.picture,
+          role: userRole, // any user logged in will be a normal user by default
+        };
+      },
+    },
+  },
+  plugins: [
+    admin({
+      adminRole: [adminRole],
+      defaultRole: userRole,
     }),
-    socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-            mapProfileToUser: (profile) => {
-                return {
-                    email: profile.email,
-                    name: profile.name,
-                    image: profile.picture,
-                    role: userRole // any user logged in will be a normal user by default
-                };
-            }
-        }
-    }
+    nextCookies(),
+  ],
 });
