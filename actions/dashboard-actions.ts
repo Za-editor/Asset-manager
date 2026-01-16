f9ea9f86c5ae8beaa5ec458c76cd1772efb7cb61
Tajudeen Fawas <frankokonkwo24@gmail.com>
2026-01-16 14:20:17 +0100

    working

--- actions/dashboard-actions.ts
+++ actions/dashboard-actions.ts
@@ -1,12 +1,80 @@
-"use server"
+"use server";
 
-import { db } from "@/lib/db"
-import { category } from "@/lib/db/schema"
+import { auth } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { asset, category } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
+import { headers } from "next/headers";
+import { z } from "zod";
+
+const AssetSchema = z.object({
+  title: z.string(),
+  description: z.string().optional(),
+  categoryId: z.number().positive("Please select a category"),
+  fileUrl: z.string().url("Invalid file Url"),
+  thumbnailUrl: z.string().url("Invalid file Url").optional(),
+});
 
 export async function getCategoriesAction() {
-    try {
-        return db.select().from(category)
-    } catch (error) {
-       console.log(error)
-    }
+  try {
+    return db.select().from(category);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function uploadAsset(formData: FormData) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    throw new Error("You must be logged in to upload asset");
+  }
+
+  try {
+    const validateFields = AssetSchema.parse({
+      title: formData.get("title"),
+      description: formData.get("description"),
+      categoryId: formData.get("categoryId"),
+      fileUrl: formData.get("fileUrl"),
+      thumbnailUrl: formData.get("thumbnailUrl") || formData.get("fileUrl"),
+    });
+
+    await db.insert(asset).values({
+      title: validateFields.title,
+      description: validateFields.description,
+      fileUrl: validateFields.fileUrl,
+      thumbnailUrl: validateFields.thumbnailUrl,
+      isApproved: "pending",
+      userId: session.user.id,
+      categoryId: validateFields.categoryId,
+    });
+
+    revalidatePath("/dashboard/assets");
+
+    return {
+      success: true,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      error: "Failed to upload Asset",
+    };
+  }
+}
+
+export async function getUserAssetsAction(userid: string) {
+  try {
+    return db
+      .select()
+      .from(asset)
+      .where(eq(asset.userId, userid))
+      .orderBy(asset.createdAt);
+  } catch (error) {
+      return [];
+      console.log(error);
+      
+  }
 }
--- app/dashboard/assets/page.tsx
+++ app/dashboard/assets/page.tsx
@@ -1,17 +1,33 @@
-import { getCategoriesAction } from "@/actions/dashboard-actions";
+import {
+  getCategoriesAction,
+  getUserAssetsAction,
+} from "@/actions/dashboard-actions";
 import AssetGrid from "@/components/dashboard/asset-grid";
 import UploadAsset from "@/components/dashboard/upload-asset";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
 async function UserAssetsPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session === null) return null;
+  const [categories, assets] = await Promise.all([
+    getCategoriesAction(),
+    getUserAssetsAction(session?.user.id),
+  ]);
 
-  const [categories] = await Promise.all([getCategoriesAction()])
-  return <div className="container py-6">
-    <div className="flex justify-between items-center mb-6">
-      <h1 className="text-3xl font-extrabold">My Assets</h1>
-      <UploadAsset categories={categories || []}/>
+  console.log(assets, "assets");
+  
+  return (
+    <div className="container py-6">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-3xl font-extrabold">My Assets</h1>
+        <UploadAsset categories={categories || []} />
+      </div>
+      <AssetGrid />
     </div>
-    <AssetGrid />
-  </div>;
+  );
 }
 
 export default UserAssetsPage;
